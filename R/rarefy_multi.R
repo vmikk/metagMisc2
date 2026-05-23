a4fc9a074baf71acd8b950dc80153fb523b833f6
Vladimir Mikryukov <v.mikryukov@gmail.com>
2026-05-23 21:05:46 +0300

    add `rarefy_multi`

---
+++ R/rarefy_multi.R
@@ -0,0 +1,21 @@
+rarefy_multi <- function(x,
+  depth     = NULL,
+  n_iter    = 100L,
+  metrics   = c("richness", "shannon", "hill1", "hill2"),
+  kernel    = c("hypergeometric", "permutation"),
+  samples_are_rows = FALSE) {
+  kernel_code <- if (identical(kernel, "hypergeometric")) 0L else 1L
+  mat <- as_rarefy_matrix(x, samples_are_rows = samples_are_rows)
+  cn <- colnames(mat)
+  cs <- Matrix::colSums(mat)
+  cn <- colnames(mat)
+  depths <- as.integer(depth)
+  allm   <- .alpha_metric_names()
+  countm <- .count_alpha_metric_names()
+  res <- rarefy_alpha_cpp(
+    mat,
+    depths,
+    as.integer(n_iter),
+    as.integer(kernel_code)
+  )
+}
